d9d42ca6ec90612482f4e6be5cdc19210d9c303f
Aleksandar Stojanovic <sale4319@gmail.com>
2025-03-12 19:31:03 +0100

    feat: add basic grid layout and display basic grid items

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -1,3 +1,20 @@
-export function GridItem() {
-  return <>GridItem</>;
+import styles from "./GridItem.module.css";
+
+type GridItemProps = {
+  posterUrl: string;
+  title: string;
+  date?: string;
+  key: number;
+};
+
+export function GridItem({ posterUrl, date, title, key }: GridItemProps) {
+  return (
+    <div className={styles.gridContainer}>
+      <div key={key} className={styles.gridItem}>
+        <img src={posterUrl} alt={title} />
+        <h3>{title}</h3>
+        <div className={styles.date}>{date}</div>
+      </div>
+    </div>
+  );
 }
