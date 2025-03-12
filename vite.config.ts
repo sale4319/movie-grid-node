51dd4ca19da58c35c280edd2bf862c46005b9df8
Aleksandar Stojanovic <sale4319@gmail.com>
2025-03-12 21:50:29 +0100

    test: deploy to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/movie-grid-node/",
   plugins: [react()],
   test: {
     environment: "jsdom",
