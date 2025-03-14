231666673ab8b15b393df3590acc3389ed60ec86
Aleksandar Stojanovic <sale4319@gmail.com>
2025-03-14 08:45:05 +0100

    feat: add load more button, and adjust view port with keyboard navigation

--- src/components/MainGrid/MainGrid.tsx
+++ src/components/MainGrid/MainGrid.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { GridItem } from "../../components";
 import { BASE_URL, FILE_SIZE } from "../../constants";
 
@@ -6,8 +6,11 @@ import movies from "../../mocks/moviesFixture.json";
 import styles from "./MainGrid.module.css";
 
 export default function MainGrid() {
+  const itemRefs = useRef<(HTMLDivElement | null)[]>([]);
   const [selected, setSelected] = useState<number | null>(null);
   const [favorites, setFavorites] = useState<number[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage = 18;
   const columns = 6;
 
   const handleSelect = (id: number) => {
@@ -27,6 +30,9 @@ export default function MainGrid() {
       (movie, index, self) => index === self.findIndex((m) => m.id === movie.id)
     )
     .sort((a, b) => b.ratings[0].rating - a.ratings[0].rating);
+
+  const totalPages = Math.ceil(uniqueMovies.length / itemsPerPage);
+  const currentMovies = uniqueMovies.slice(0, currentPage * itemsPerPage);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -67,21 +73,51 @@ export default function MainGrid() {
     };
   }, [selected, uniqueMovies]);
 
+  useEffect(() => {
+    if (selected !== null) {
+      const selectedIndex = uniqueMovies.findIndex(
+        (movie) => movie.id === selected
+      );
+      if (itemRefs.current[selectedIndex]) {
+        itemRefs.current[selectedIndex]?.scrollIntoView({
+          behavior: "smooth",
+          block: "center",
+        });
+      }
+    }
+  }, [selected, uniqueMovies]);
+
+  const handleLoadMore = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
   return (
-    <div data-testid="main-grid" className={styles.mainGrid}>
-      {uniqueMovies.map((item, index) => (
-        <GridItem
-          key={index}
-          title={item.title}
-          date={item.release_date}
-          posterUrl={`${BASE_URL}${FILE_SIZE}${item.poster_path}`}
-          backdropUrl={`${BASE_URL}${FILE_SIZE}${item.backdrop_path}`}
-          isSelected={selected === item.id}
-          inFavorites={favorites.includes(item.id)}
-          toggleFavourite={() => toggleFavourite(item.id)}
-          onSelect={() => handleSelect(item.id)}
-        />
-      ))}
-    </div>
+    <>
+      <div data-testid="main-grid" className={styles.mainGrid}>
+        {currentMovies.map((item, index) => (
+          <GridItem
+            key={index}
+            ref={(el) => {
+              itemRefs.current[index] = el;
+            }}
+            title={item.title}
+            date={item.release_date}
+            posterUrl={`${BASE_URL}${FILE_SIZE}${item.poster_path}`}
+            backdropUrl={`${BASE_URL}${FILE_SIZE}${item.backdrop_path}`}
+            isSelected={selected === item.id}
+            inFavorites={favorites.includes(item.id)}
+            toggleFavourite={() => toggleFavourite(item.id)}
+            onSelect={() => handleSelect(item.id)}
+          />
+        ))}
+      </div>
+      {currentPage < totalPages && (
+        <div className={styles.loadMore}>
+          <button tabIndex={0} onClick={handleLoadMore}>
+            Load More
+          </button>
+        </div>
+      )}
+    </>
   );
 }
