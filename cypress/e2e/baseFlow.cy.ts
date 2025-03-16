0c82286ea663d4e0439e83e06bf9ce12587caebb
Aleksandar Stojanovic <sale4319@gmail.com>
2025-03-16 06:32:46 +0100

    test: add e2e for keyboard navigation

--- cypress/e2e/baseFlow.cy.ts
+++ cypress/e2e/baseFlow.cy.ts
@@ -6,10 +6,30 @@ describe("Base flow e2e test", () => {
 
     cy.get('[data-testid="grid-item"]')
       .eq(2)
-      .should("contain.text", "The Godfather");
+      .should(($el) => {
+        expect($el.attr("class")).to.contain("selected");
+      });
 
     cy.get('[data-testid="favourite-icon-outlined"]').eq(2).click();
 
+    cy.get('[data-testid="favourite-icon-filled"]').should("exist");
+  });
+
+  it("should navigate using keyboard and select and favour an item", () => {
+    cy.visit("https://sale4319.github.io/movie-grid-node/");
+
+    cy.get('[data-testid="grid-item"]').eq(0).click();
+
+    cy.get("body").type(
+      "{rightarrow}{downarrow}{downarrow}{rightarrow}{leftarrow}{uparrow}"
+    );
+
+    cy.get("body").type("{enter}");
+    cy.get('[data-testid="grid-item"]')
+      .eq(7)
+      .should(($el) => {
+        expect($el.attr("class")).to.contain("selected");
+      });
     cy.get('[data-testid="favourite-icon-filled"]').should("exist");
   });
 });
